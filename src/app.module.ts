36e7890e2b041fbba26480f32240ec9c7e6e4130
Soonyoung Park <soon.syp@gmail.com>
2025-06-29 01:26:42 +0200

    clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ProductsModule } from './products/products.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
